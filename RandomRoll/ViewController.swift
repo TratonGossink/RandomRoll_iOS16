431c8f72657411ebc7b220c4c0278e75ce96d0dc
Traton Gossink <79613749+TratonGossink@users.noreply.github.com>
2023-03-27 17:50:05 -0500

    Initial Commit

---
+++ RandomRoll/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  RandomRoll
+//
+//  Created by Traton Gossink on 3/27/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
